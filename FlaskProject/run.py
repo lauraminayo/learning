86de1390fdaf4bd999c6227a373823ae135a5818
lauraminayo <minayolaura1@gmail.com>
2022-04-17 04:17:33 -0700

    creating a package for the project

--- FlaskProject/run.py
+++ FlaskProject/run.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' 
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+from market import app
 
-db = SQLAlchemy(app)
+#Checks if the run.py file has executed directly and not imported
+if __name__ == '__main__':
+    app.run(debug=True)
