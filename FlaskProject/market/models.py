57ffd9093930a064b6ba20f1c736f369f49cf1c9
lauraminayo <minayolaura1@gmail.com>
2022-04-18 12:08:00 -0700

    created a db on the terminal

--- FlaskProject/market/models.py
+++ FlaskProject/market/models.py
@@ -1,15 +1,15 @@
 from market import db
 
-class User(db.model):
-     id = db.Column(db.Integer(),primary_key=True)
+class User(db.Model):
+     id = db.Column(db.Integer(), primary_key=True)
      username = db.Column(db.String(length=30), nullable=False, unique=True)
      email_address = db.Column(db.String(length=50), nullable= False, unique=True)
      password_hash = db.Column(db.String(length=60), nullable =False)
      budget = db.Column(db.Integer(), nullable=False, default=1000)
-     items = db.relationship('Item', backref='owned_user, lazy=True')
+     items = db.relationship('Item', backref='owned_user', lazy=True)
 
 class Item(db.Model):
-    id = db.Column(db.Integer(),primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length =30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length = 12), nullable=False, unique=True)
@@ -17,4 +17,4 @@ class Item(db.Model):
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return f'Item{self.name}'
+        return f'Item {self.name}'
--- FlaskProject/market/routes.py
+++ FlaskProject/market/routes.py
@@ -1,6 +1,7 @@
 from market import app
 from flask import render_template
 from market.models import Item
+
 @app.route('/')
 @app.route('/home')
 def home_page():
