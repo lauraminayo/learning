e5d685eb8ceabc96ec8faffd5cfe3cd5c7cb5725
lauraminayo <minayolaura1@gmail.com>
2022-04-20 01:59:12 -0700

    created a secret key

--- FlaskProject/market/__init__.py
+++ FlaskProject/market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' 
+app.config['SECRET_KEY'] = 'd0e60a3bce6d7b3d0b0dad88'
 
 
 
