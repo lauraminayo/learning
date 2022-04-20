e0dc30d87a903b37f0bf34524dbdef1f4b47fe64
lauraminayo <minayolaura1@gmail.com>
2022-04-20 00:33:26 -0700

    dropped all data put in the db

--- FlaskProject/market/models.py
+++ FlaskProject/market/models.py
@@ -15,6 +15,5 @@ class Item(db.Model):
     barcode = db.Column(db.String(length = 12), nullable=False, unique=True)
     description = db.Column(db.String(length = 1024), nullable=False, unique=True)
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
-
     def __repr__(self):
         return f'Item {self.name}'
