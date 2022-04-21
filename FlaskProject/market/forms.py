9d5fd1156d331d28f6917d0275daf93c77b9b5ef
lauraminayo <minayolaura1@gmail.com>
2022-04-21 11:59:20 -0700

    importing a redirect for the user to the marlet page

--- FlaskProject/market/forms.py
+++ FlaskProject/market/forms.py
@@ -5,6 +5,6 @@ from wtforms import StringField, PasswordField, SubmitField
 class RegisterForm(FlaskForm):
     username = StringField(label ='User Name:')
     email_address = StringField(label='Email Address:')
-    password1 =  PasswordField(label='Password')
-    password2 =  PasswordField(label='Confirm Password')
-    submit = SubmitField(label='submit')
+    password1 =  PasswordField(label='Password:')
+    password2 =  PasswordField(label='Confirm Password:')
+    submit = SubmitField(label='Create Account')
