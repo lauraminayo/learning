ef67251e96bdeab004381cb75f6cdc07e940a471
lauraminayo <minayolaura1@gmail.com>
2022-04-24 12:24:29 -0700

    created an alert message

--- FlaskProject/market/routes.py
+++ FlaskProject/market/routes.py
@@ -1,5 +1,5 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm
 from market import db
@@ -27,7 +27,7 @@ def register_page():
 
     if form.errors != {}: #If there are no errors from the validation
        for err_msg in form.errors.values():
-        print(f'There was an error in creating the user: {err_msg}')
+        flash(f'There was an error in creating the user: {err_msg}',category='danger')
 
     return render_template('register.html', form=form)
     
