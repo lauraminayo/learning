8a0bb723c627e6fba54402eba8cbdc8466aaca23
lauraminayo <minayolaura1@gmail.com>
2022-04-28 11:09:45 -0700

    validating email and username

--- FlaskProject/market/forms.py
+++ FlaskProject/market/forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
-
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
+    def validate_username(self, username_to_check):
+       username = User.query.filter_by(username = username_to_check.data).first()
+       if username:
+           raise ValidationError('Username already exists! Please try another username')
+
+    def validate_email_address(self, email_address_to_check):
+       email_address = User.query.filter_by(email_address = email_address_to_check.data).first()
+       if email_address:
+           raise ValidationError('Email Address already exists! Please try another valid address')
+   
     username = StringField(label ='User Name:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
     password1 =  PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
