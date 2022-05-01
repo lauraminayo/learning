e271381d15ee60928fe6bdb6463ddf2346c3c998
lauraminayo <minayolaura1@gmail.com>
2022-05-01 08:05:54 -0700

    implementing the log In page

--- FlaskProject/market/routes.py
+++ FlaskProject/market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LogInForm
 from market import db
 
 @app.route('/')
@@ -33,6 +33,8 @@ def register_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
-    return render_template('login.html')
+    form = LogInForm()
+
+    return render_template('login.html', form=form)
         
     
