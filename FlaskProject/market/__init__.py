be6670b953f6f945e6c7fbc509729b0c35731367
lauraminayo <minayolaura1@gmail.com>
2022-05-01 06:48:26 -0700

    user authentication

--- FlaskProject/market/__init__.py
+++ FlaskProject/market/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' 
@@ -8,5 +10,6 @@ app.config['SECRET_KEY'] = 'd0e60a3bce6d7b3d0b0dad88'
 
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
--- FlaskProject/market/routes.py
+++ FlaskProject/market/routes.py
@@ -20,7 +20,7 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
-                              password_hash=form.password1.data)
+                              password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
@@ -30,4 +30,9 @@ def register_page():
         flash(f'There was an error in creating the user: {err_msg}',category='danger')
 
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    return render_template('login.html')
+        
     
