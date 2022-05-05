afe49f769b9c5d30c3d61854b9f4e4900764875d
lauraminayo <minayolaura1@gmail.com>
2022-05-05 03:39:29 -0700

    logouts and customization

--- FlaskProject/market/__init__.py
+++ FlaskProject/market/__init__.py
@@ -3,17 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' 
-app.config['SECRET_KEY'] = 'd0e60a3bce6d7b3d0b0dad88'
-
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-
-
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import routes
--- FlaskProject/market/routes.py
+++ FlaskProject/market/routes.py
@@ -1,9 +1,10 @@
+from unicodedata import category
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -11,6 +12,7 @@ def home_page():
     return render_template("index.html")
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
@@ -26,6 +28,8 @@ def register_page():
         )
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created succesfully! You are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('market_page'))
 
     if form.errors != {}: #If there are no errors from the validation
@@ -49,5 +53,12 @@ def login_page():
             flash('Username and password are not a match! Please try again', category='danger')
 
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect( url_for('home_page'))
+
         
     
